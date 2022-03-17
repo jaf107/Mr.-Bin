324b1aff9a971901c52da93fc19f3d12b9ea69c2
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-03-17 10:55:23 +0600

    Removed unncessary files

---
+++ mr-bin/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Mr. Bin
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
