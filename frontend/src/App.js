70397c7ec6a7bc696f81d7d577cc143778ee4011
jsureka <bsse1115@iit.du.ac.bd>
2022-04-05 21:14:54 +0600

    Login and Register Page Creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './css/App.css';
 import Login from './components/Login';
+import Register from './components/Register';
+import { Route, Routes} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-        <Login></Login> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Login/>}></Route>
+          <Route path='/register' element={<Register/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Outlet></Outlet>
     </div>
   );
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,43 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../css/Login.css";
+
 class Login extends React.Component {
   state = {};
   render() {
     return (
-      <div class="container">
-      <div class="row m-5  shadow-lg">
-      <div class="col-md-6 d-none d-md-block">
-      <img src={require('../assets/bg_1.jpg')} class="img-fluid"  />
-      </div>
-      <div class="col-md-6 bg-white p-5">
-      <h3 class="pb-3 text-center fw-bold">Login to Mr. Bin</h3>
-      <div class="form-style">
-      <form>
-        <div class="form-group pb-3">    
-          <input type="email" placeholder="Email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>   
+      <div class="container pb-5">
+        <div class="row m-5  shadow-lg">
+          <div class="col-md-6 d-none d-md-block">
+            <img src={require("../assets/login.jpg")} class="img-fluid" />
+          </div>
+          <div class="col-md-6 bg-white p-4">
+            <h3 class="pb-3 text-center fw-bold mt-5">SIGN IN</h3>
+            <div class="form-style p-5">
+              <form>
+                <div class="form-group pb-3">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div class="form-group pb-3">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="d-flex align-items-center">
+                    <input name="" type="checkbox" value="" />{" "}
+                    <span class="pl-2 fw-light">Remember Me</span>
+                  </div>
+                  <div>
+                    <a href="#" class="text-decoration-none text-danger">Forget Password?</a>
+                  </div>
+                </div>
+                <div class="pb-2 text-center mt-4">
+                  <button
+                    type="submit"
+                    class="btn btn-success w-50"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+              <div class="sideline">OR</div>
+              <div class="text-center">
+                <button type="button" class="login-with-google-btn w-50">
+                  Sign in with Google
+                </button>{" "}
+              </div>
+              <div class="pt-4 text-center">
+                Dont have an Account? <Link to="/register" class=" text-success text-decoration-none fw-bold">Sign Up</Link>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="form-group pb-3">   
-          <input type="password" placeholder="Password" class="form-control" id="exampleInputPassword1"/>
-        </div>
-        <div class="d-flex align-items-center justify-content-between">
-      <div class="d-flex align-items-center"><input name="" type="checkbox" value="" /> <span class="pl-2 font-weight-bold">Remember Me</span></div>
-      <div><a href="#">Forget Password?</a></div>
-      </div>
-         <div class="pb-2">
-        <button type="submit" class="btn btn-success w-100 font-weight-bold mt-2">Login</button>
-         </div>
-      </form>
-        <div class="sideline">OR</div>
-        <div>
-        <button type="submit" class="btn btn-primary w-100 font-weight-bold mt-2"><i class="fa fa-facebook" aria-hidden="true"></i> Login With Facebook</button>
-        </div>
-        <div class="pt-4 text-center">
-        Dont have an Account? <a href="#">Sign Up</a>
-        </div>
-      </div>
-      
-      </div>
-      </div>
       </div>
     );
   }
