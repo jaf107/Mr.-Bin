a61418204813ff4babbf96a79f3b41d5cbccc092
jsureka <bsse1115@iit.du.ac.bd>
2022-04-12 02:08:26 +0600

    registration reducers and actions added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -57,3 +57,21 @@ import {
       dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
     }
   };
+
+  // Register
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST });
+
+    const config = { headers: { "Content-Type": "multipart/form-data" } };
+
+    const { data } = await axios.post(`/api/v1/register`, userData, config);
+
+    dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
--- frontend/src/components/User/Register.js
+++ frontend/src/components/User/Register.js
@@ -1,81 +1,127 @@
-import React from "react";
+import {React, useState} from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { register } from "../../actions/userActions";
+function Register({ history, location }) {
+  const dispatch = useDispatch();
 
-class Register extends React.Component {
-  state = {};
-  render() {
-    return (
-      <div class="container pb-5">
-        <div class="row m-5  shadow-lg">
-          <div class="col-md-6 d-none d-md-block">
-            <img src={require("../../assets/register.jpg")} class="img-fluid" alt="register" />
-          </div>
-          <div class="col-md-6 bg-white p-4">
-            <h3 class="pb-3 text-center fw-bold mt-5">REGISTER</h3>
-            <div class="form-style p-5">
-              <form>
-                <div class="form-group pb-3">
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <div class="form-group pb-3">
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <div class="form-group pb-3">
-                  <input
-                    type="text"
-                    placeholder="Phone Number"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <div class="form-group pb-3">
-                  <input
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = user;
+  const registerSubmit = (e) => {
+    e.preventDefault();
+
+    const myForm = new FormData();
+
+    myForm.set("name", name);
+    myForm.set("email", email);
+    myForm.set("password", password);
+    dispatch(register(myForm));
+  };
+
+  const registerDataChange = (e) => {
+    if (e.target.name === "avatar") {
+      const reader = new FileReader();
+
+      // reader.onload = () => {
+      //   if (reader.readyState === 2) {
+      //     setAvatarPreview(reader.result);
+      //     setAvatar(reader.result);
+      //   }
+      // };
+
+      reader.readAsDataURL(e.target.files[0]);
+    } else {
+      setUser({ ...user, [e.target.name]: e.target.value });
+    }
+  };
+
+  return (
+    <div class="container pb-5">
+      <div class="row m-5  shadow-lg">
+        <div class="col-md-6 d-none d-md-block">
+          <img
+            src={require("../../assets/register.jpg")}
+            class="img-fluid"
+            alt="register"
+          />
+        </div>
+        <div class="col-md-6 bg-white p-4">
+          <h3 class="pb-3 text-center fw-bold mt-5">REGISTER</h3>
+          <div class="form-style p-5">
+            <form onSubmit={registerSubmit}>
+              <div class="form-group pb-3">
+                <input
+                  ype="text"
+                  placeholder="Name"
+                  required
+                  name="name"
+                  value={name}
+                  onChange={registerDataChange}
+                />
+              </div>
+              <div class="form-group pb-3">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  required
+                  name="email"
+                  value={email}
+                  onChange={registerDataChange}
+                />
+              </div>
+              {/* <div class="form-group pb-3">
+                <input
+                  type="text"
+                  placeholder="Phone Number"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div> */}
+              <div class="form-group pb-3">
+                <input
                     type="password"
                     placeholder="Password"
-                    class="form-control"
-                    id="exampleInputPassword1"
-                  />
-                </div>
-                <div class="form-group pb-3">
-                  <input
-                    type="password"
-                    placeholder="Confirm Password"
-                    class="form-control"
-                    id="exampleInputPassword1"
-                  />
-                </div>
-
-                <div class="pb-2 text-center mt-4">
-                  <button type="submit" class="btn btn-success w-50">
-                    Register
-                  </button>
-                </div>
-              </form>
-              <div class="pt-4 text-center">
-                Already have an Account?{" "}
-                <Link to="/login" class=" text-success text-decoration-none fw-bold">
-                  Sign In
-                </Link>
+                    required
+                    name="password"
+                    value={password}
+                    onChange={registerDataChange}
+                />
               </div>
+              {/* <div class="form-group pb-3">
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                />
+              </div> */}
+
+              <div class="pb-2 text-center mt-4">
+                <button type="submit" class="btn btn-success w-50">
+                  Register
+                </button>
+              </div>
+            </form>
+            <div class="pt-4 text-center">
+              Already have an Account?{" "}
+              <Link
+                to="/login"
+                class=" text-success text-decoration-none fw-bold"
+              >
+                Sign In
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Register;
