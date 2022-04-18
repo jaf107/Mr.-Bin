38f1828d371d862610568b4df121b6d427dada28
Jaf107 <bsse1109@iit.du.ac.bd>
2022-04-18 21:29:55 +0600

    Sample product used

--- frontend/src/components/Marketplace/Marketplace.js
+++ frontend/src/components/Marketplace/Marketplace.js
@@ -4,63 +4,44 @@ import Footer from '../Footer/Footer'
 import "./Marketplace.css";
 
 const Marketplace = () => {
+  let product = {
+    name: "Laptop",
+    price: "500usd"
+  };
+  let products = [{
+    name: "Wooden Table",
+    price: "50$"
+  },
+  {
+    name: "Blender",
+    price: "150$"
+  },{
+    name: "Samsung Galaxy M21",
+    price: "250$"
+  },
+];
+
   return (
 
     <div className='marketplace'>
       <Header/>
-      <nav class="navbar navbar-expand-sm navbar-light bg-white border-bottom"> <a class="navbar-brand ml-2 font-weight-bold" href="#">FORCEPAUSED</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor" aria-controls="navbarColor" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-        <div class="collapse navbar-collapse" id="navbarColor">
-          <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="#">Women's</a> </li>
-            <li class="nav-item"><a class="nav-link" href="#">Men's</a> </li>
-            <li class="nav-item "><a class="nav-link" href="#">Home</a> </li>
-            <li class="nav-item "><a class="nav-link" href="#">Sale</a> </li>
-          </ul>
-        </div>
-      </nav>
+      
       <div class="filter"> <button class="btn btn-default" type="button" data-toggle="collapse" data-target="#mobile-filter" aria-expanded="false" aria-controls="mobile-filter">Filters<span class="fa fa-filter pl-1"></span></button>
       </div>
-      <div id="mobile-filter">
-        <div>
-          <h6 class="p-1 border-bottom">Home Furniture</h6>
-          <ul>
-            <li><a href="#">Living</a></li>
-            <li><a href="#">Dining</a></li>
-            <li><a href="#">Office</a></li>
-            <li><a href="#">Bedroom</a></li>
-            <li><a href="#">Kitchen</a></li>
-          </ul>
-        </div>
-        <div>
-          <h6 class="p-1 border-bottom">Filter By</h6>
-          <p class="mb-2">Color</p>
-          <ul class="list-group">
-            <li class="list-group-item list-group-item-action mb-2 rounded"><a href="#"> <span class="fa fa-circle pr-1" id="red"></span>Red </a></li>
-            <li class="list-group-item list-group-item-action mb-2 rounded"><a href="#"> <span class="fa fa-circle pr-1" id="teal"></span>Teal </a></li>
-            <li class="list-group-item list-group-item-action mb-2 rounded"><a href="#"> <span class="fa fa-circle pr-1" id="blue"></span>Blue </a></li>
-          </ul>
-        </div>
-        <div>
-          <h6>Type</h6>
-          <form class="ml-md-2">
-            <div class="form-inline border rounded p-sm-2 my-2"> <input type="radio" name="type" id="boring" /> <label for="boring" class="pl-1 pt-sm-0 pt-1">Boring</label> </div>
-            <div class="form-inline border rounded p-sm-2 my-2"> <input type="radio" name="type" id="ugly" /> <label for="ugly" class="pl-1 pt-sm-0 pt-1">Ugly</label> </div>
-            <div class="form-inline border rounded p-md-2 p-sm-1"> <input type="radio" name="type" id="notugly" /> <label for="notugly" class="pl-1 pt-sm-0 pt-1">Not Ugly</label> </div>
-          </form>
-        </div>
-      </div>
+      
       <section id="sidebar">
         <div>
-          <h6 class="p-1 border-bottom">Home Furniture</h6>
+          <h6 class="p-1 border-bottom">Commodity types</h6>
           <ul>
-            <li><a href="#">Living</a></li>
-            <li><a href="#">Dining</a></li>
-            <li><a href="#">Office</a></li>
-            <li><a href="#">Bedroom</a></li>
-            <li><a href="#">Kitchen</a></li>
+            <li><a href="#">Books</a></li>
+            <li><a href="#">Newspaper</a></li>
+            <li><a href="#">Clothes</a></li>
+            <li><a href="#">Plastic</a></li>
+            <li><a href="#">Glassware</a></li>
           </ul>
         </div>
-        <div>
+
+        {/* <div>
           <h6 class="p-1 border-bottom">Filter By</h6>
           <p class="mb-2">Color</p>
           <ul class="list-group">
@@ -69,12 +50,14 @@ const Marketplace = () => {
             <li class="list-group-item list-group-item-action mb-2 rounded"><a href="#"> <span class="fa fa-circle pr-1" id="blue"></span>Blue </a></li>
           </ul>
         </div>
+         */}
         <div>
           <h6>Type</h6>
           <form class="ml-md-2">
-            <div class="form-inline border rounded p-sm-2 my-2"> <input type="radio" name="type" id="boring" /> <label for="boring" class="pl-1 pt-sm-0 pt-1">Boring</label> </div>
-            <div class="form-inline border rounded p-sm-2 my-2"> <input type="radio" name="type" id="ugly" /> <label for="ugly" class="pl-1 pt-sm-0 pt-1">Ugly</label> </div>
-            <div class="form-inline border rounded p-md-2 p-sm-1"> <input type="radio" name="type" id="notugly" /> <label for="notugly" class="pl-1 pt-sm-0 pt-1">Not Ugly</label> </div>
+            <div class="form-inline border rounded p-sm-2 my-2"> <input type="radio" name="type" id="boring" /> <label for="boring" class="pl-1 pt-sm-0 pt-1">Direct Buy</label> </div>
+            <div class="form-inline border rounded p-sm-2 my-2"> <input type="radio" name="type" id="ugly" /> <label for="ugly" class="pl-1 pt-sm-0 pt-1">Bidding</label> </div>
+            <div class="form-inline border rounded p-md-2 p-sm-1"> <input type="radio" name="type" id="notugly" /> <label for="notugly" class="pl-1 pt-sm-0 pt-1">
+            Wholesale  </label> </div>
           </form>
         </div>
       </section>
@@ -84,8 +67,11 @@ const Marketplace = () => {
             <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
               <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/963486/pexels-photo-963486.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
                 <div class="card-body">
-                  <p class="card-text">Wooden chair with legs</p>
-                  <p>$90</p> <span class="fa fa-circle" id="red"></span> <span class="fa fa-circle" id="teal"></span> <span class="fa fa-circle" id="blue"></span>
+                  <p class="card-text">{product.name} </p>
+                  <p>{product.price} </p> 
+                  {/* <span class="fa fa-circle" id="red"></span> 
+                  <span class="fa fa-circle" id="teal"></span> 
+                  <span class="fa fa-circle" id="blue"></span> */}
                 </div>
               </div>
             </div>
