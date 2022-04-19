d99038b8f4bc4ea51b99196215b52cec224d0182
Jaf107 <bsse1109@iit.du.ac.bd>
2022-04-19 09:18:34 +0600

    Changed Marketplace

--- frontend/src/components/Marketplace/Marketplace.js
+++ frontend/src/components/Marketplace/Marketplace.js
@@ -8,9 +8,10 @@ import { Link } from "react-router-dom";
 const Marketplace = () => {
   let product = {
     name: "Laptop",
-    price: "500usd"
+    price: "$ 500",
+    description: "Used for 10 months. Urgent sell. No issues"
   };
-  
+
 
   return (
 
@@ -50,9 +51,9 @@ const Marketplace = () => {
           <div>
             <h6 className=' p-3'>Type</h6>
             <form class="ml-md-2">
-              <div class="form-inline border rounded p-sm-2 my-2 p-2"> <input type="radio" name="type" id="boring" /> <label for="boring" class="pl-1 pt-sm-0 pt-1">Direct Buy</label> </div>
-              <div class="form-inline border rounded p-sm-2 my-2 p-2"> <input type="radio" name="type" id="ugly" /> <label for="ugly" class="pl-1 pt-sm-0 pt-1">Bidding</label> </div>
-              <div class="form-inline border rounded p-md-2 p-sm-1 p-2"> <input type="radio" name="type" id="notugly" /> <label for="notugly" class="pl-1 pt-sm-0 pt-1">
+              <div class="form-inline border rounded p-sm-2 my-2 p-2"> <input type="radio" name="type" id="direct_buy" /> <label for="direct_buy" class="pl-1 pt-sm-0 pt-1">Direct Buy</label> </div>
+              <div class="form-inline border rounded p-sm-2 my-2 p-2"> <input type="radio" name="type" id="bidding" /> <label for="bidding" class="pl-1 pt-sm-0 pt-1">Bidding</label> </div>
+              <div class="form-inline border rounded p-md-2 p-sm-1 p-2"> <input type="radio" name="type" id="wholesale" /> <label for="wholesale" class="pl-1 pt-sm-0 pt-1">
                 Wholesale  </label> </div>
             </form>
           </div>
@@ -60,81 +61,179 @@ const Marketplace = () => {
         <section id="products">
           <div class="container">
             <div class="row">
+
               <h3 className='p-4'>Marketplace</h3>
-              <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
-                <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/963486/pexels-photo-963486.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+
+              <div class="col-lg-3 col-sm-6 col-11 offset-sm-0 offset-1">
+                <div class="card">
+                  <img
+                    class="card-img-top"
+                    src={require("../../assets/laptop.jpg")}
+                    alt="Card image cap"
+                  />
                   <div class="card-body">
-                    <p class="card-text text-center p-2">{product.name} </p>
-                    <p className=' text-center p-2'>{product.price} </p>
-                    {/* <span class="fa fa-circle" id="red"></span> 
-                  <span class="fa fa-circle" id="teal"></span> 
-                  <span class="fa fa-circle" id="blue"></span> */}
+
+                    <h5 className='card-title' >{product.name}</h5>
+                    <p class="card-text p-2">
+                      {product.description} </p>
+                    <p className='card-text text-center p-2'>
+                      Price: {product.price}
+                    </p>
                     <div className="d-flex">
                       <div className="flex-fill border-0 btn btn-success">Bid</div>
                       <div className="flex-fill border-0 btn btn-danger"><i className=' fas fa-heart'></i></div>
                       <div className="flex-fill border-0 btn btn-primary"> Buy</div>
                     </div>
+                    {/* <div className="d-flex">
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-success card-button'>
+                          Bid
+                        </button>
+                      </div>
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-danger card-button'>
+                          <i className=' fas fa-heart'></i></button> </div>
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-primary border-0 card-button'>
+                          Buy
+                        </button>
+                      </div>
+
+                    </div> */}
+
+
                   </div>
                 </div>
               </div>
-              <div class="col-lg-3 offset-lg-0 col-sm-4 offset-sm-2 col-11 offset-1">
-                <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/1125137/pexels-photo-1125137.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+              
+              <div class="col-lg-3 col-sm-6 col-11 offset-sm-0 offset-1">
+                <div class="card">
+                  <img
+                    class="card-img-top"
+                    src={require("../../assets/laptop.jpg")}
+                    alt="Card image cap"
+                  />
                   <div class="card-body">
-                    <p class="card-text">Ugly chair and table set</p>
-                    <p>$100</p> <span class="fa fa-circle" id="red"></span> <span class="fa fa-circle" id="teal"></span> <span class="fa fa-circle" id="blue"></span>
+
+                    <h5 className='card-title' >{product.name}</h5>
+                    <p class="card-text p-2">
+                      {product.description} </p>
+                    <p className='card-text text-center p-2'>
+                      Price: {product.price}
+                    </p>
+                    <div className="d-flex">
+                      <div className="flex-fill border-0 btn btn-success">Bid</div>
+                      <div className="flex-fill border-0 btn btn-danger"><i className=' fas fa-heart'></i></div>
+                      <div className="flex-fill border-0 btn btn-primary"> Buy</div>
+                    </div>
+                    {/* <div className="d-flex">
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-success card-button'>
+                          Bid
+                        </button>
+                      </div>
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-danger card-button'>
+                          <i className=' fas fa-heart'></i></button> </div>
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-primary border-0 card-button'>
+                          Buy
+                        </button>
+                      </div>
+
+                    </div> */}
+
+
                   </div>
                 </div>
               </div>
-              <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
-                <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/3757055/pexels-photo-3757055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+
+              <div class="col-lg-3 col-sm-6 col-11 offset-sm-0 offset-1">
+                <div class="card">
+                  <img
+                    class="card-img-top"
+                    src={require("../../assets/laptop.jpg")}
+                    alt="Card image cap"
+                  />
                   <div class="card-body">
-                    <p class="card-text">Leather Lounger</p>
-                    <p>$950</p> <span class="fa fa-circle" id="red"></span> <span class="fa fa-circle" id="teal"></span> <span class="fa fa-circle" id="blue"></span>
+
+                    <h5 className='card-title' >{product.name}</h5>
+                    <p class="card-text p-2">
+                      {product.description} </p>
+                    <p className='card-text text-center p-2'>
+                      Price: {product.price}
+                    </p>
+                    <div className="d-flex">
+                      <div className="flex-fill border-0 btn btn-success">Bid</div>
+                      <div className="flex-fill border-0 btn btn-danger"><i className=' fas fa-heart'></i></div>
+                      <div className="flex-fill border-0 btn btn-primary"> Buy</div>
+                    </div>
+                    {/* <div className="d-flex">
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-success card-button'>
+                          Bid
+                        </button>
+                      </div>
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-danger card-button'>
+                          <i className=' fas fa-heart'></i></button> </div>
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-primary border-0 card-button'>
+                          Buy
+                        </button>
+                      </div>
+
+                    </div> */}
+
+
                   </div>
                 </div>
               </div>
-              <div class="col-lg-3 col-sm-4 offset-lg-0 offset-sm-2 col-11 offset-1">
-                <div class="card"> <img class="card-img-top" src="https://images.unsplash.com/photo-1537182534312-f945134cce34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Card image cap" />
+
+              <div class="col-lg-3 col-sm-6 col-11 offset-sm-0 offset-1">
+                <div class="card">
+                  <img
+                    class="card-img-top"
+                    src={require("../../assets/laptop.jpg")}
+                    alt="Card image cap"
+                  />
                   <div class="card-body">
-                    <p class="card-text">Tree Trunk table set</p>
-                    <p>$390</p> <span class="fa fa-circle" id="red"></span> <span class="fa fa-circle" id="teal"></span> <span class="fa fa-circle" id="blue"></span>
+
+                    <h5 className='card-title' >{product.name}</h5>
+                    <p class="card-text p-2">
+                      {product.description} </p>
+                    <p className='card-text text-center p-2'>
+                      Price: {product.price}
+                    </p>
+                    <div className="d-flex">
+                      <div className="flex-fill border-0 btn btn-success">Bid</div>
+                      <div className="flex-fill border-0 btn btn-danger"><i className=' fas fa-heart'></i></div>
+                      <div className="flex-fill border-0 btn btn-primary"> Buy</div>
+                    </div>
+                    {/* <div className="d-flex">
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-success card-button'>
+                          Bid
+                        </button>
+                      </div>
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-danger card-button'>
+                          <i className=' fas fa-heart'></i></button> </div>
+                      <div className="flex-fill border-0 ">
+                        <button className='btn btn-primary border-0 card-button'>
+                          Buy
+                        </button>
+                      </div>
+
+                    </div> */}
+
+
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="row mt-3">
-              <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
-                <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/3230274/pexels-photo-3230274.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
-                  <div class="card-body">
-                    <p class="card-text">Red Leather Bar Stool</p>
-                    <p>$30</p> <span class="fa fa-circle" id="red"></span> <span class="fa fa-circle" id="teal"></span> <span class="fa fa-circle" id="blue"></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-3 col-sm-4 offset-lg-0 offset-sm-2 col-11 offset-1">
-                <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/3773571/pexels-photo-3773571.png?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
-                  <div class="card-body">
-                    <p class="card-text">Modern Dining Table</p>
-                    <p>$740</p> <span class="fa fa-circle" id="red"></span> <span class="fa fa-circle" id="teal"></span> <span class="fa fa-circle" id="blue"></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
-                <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/534172/pexels-photo-534172.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
-                  <div class="card-body">
-                    <p class="card-text">Boring Dining Table</p>
-                    <p>$760</p> <span class="fa fa-circle" id="red"></span> <span class="fa fa-circle" id="teal"></span> <span class="fa fa-circle" id="blue"></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-3 col-sm-4 offset-lg-0 offset-sm-2 col-11 offset-1">
-                <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/37347/office-sitting-room-executive-sitting.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
-                  <div class="card-body">
-                    <p class="card-text">An Ugly Office</p>
-                    <p>$90</p> <span class="fa fa-circle" id="red"></span> <span class="fa fa-circle" id="teal"></span> <span class="fa fa-circle" id="blue"></span>
-                  </div>
-                </div>
-              </div>
+
+              
+
             </div>
           </div>
         </section>
