7aae25111ca5b7fd49ad0c54900c01c675e931e5
jsureka <bsse1115@iit.du.ac.bd>
2022-05-02 00:15:50 +0600

    phone number field corrected

--- backend/routes/recyclerRoute.js
+++ backend/routes/recyclerRoute.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const { createRecycler, getAllRecyclers } = require('../controllers/recyclerController.js');
+const express = require("express");
+const {
+  createRecycler,
+  getAllRecyclers,
+} = require("../controllers/recyclerController.js");
 
 const router = express.Router();
 
-router.route("/recycler/new")
-    .post( createRecycler)
-router.route("/recyclers")
-    .get(getAllRecyclers)
+router.route("/recycler/new").post(createRecycler);
+router.route("/recyclers").get(getAllRecyclers);
 
 module.exports = router;
