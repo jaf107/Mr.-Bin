95574edc4749882ba7216705dbb3d28759c5fa79
Jaf107 <bsse1109@iit.du.ac.bd>
2022-05-08 00:42:03 +0600

    Recycle Data

---
+++ frontend/src/data/Recycle.js
@@ -0,0 +1,49 @@
+const products = [
+    {
+        type: "Plastic",
+        items: [
+            {
+                condition: "hard",
+                price: 45
+            },
+            {
+                condition: "processed",
+                price: 17
+            }, {
+                condition: "unprocessed",
+                price: 10
+            },
+        ],
+    },
+    {
+        type: "Electronics",
+        items: [
+            {
+                condition: "broken",
+                price: 10
+            },
+            {
+                condition: "processed",
+                price: 30
+            }, {
+                condition: "unprocessed",
+                price: 15
+            },
+        ],
+    },
+    {
+        type: "Newspaper",
+        items: [
+            {
+                condition: "untorn",
+                price: 10
+            },
+            {
+                condition: "book",
+                price: 20
+            },
+        ],
+
+    },
+]
+export default products;
