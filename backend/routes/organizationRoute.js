8b21ed79fc77a76300619662f21ac19614734349
Jaf107 <bsse1109@iit.du.ac.bd>
2022-06-07 00:00:53 +0600

    Functional

--- backend/routes/organizationRoute.js
+++ backend/routes/organizationRoute.js
@@ -11,6 +11,6 @@ const router = express.Router();
 router.route("/organization/new").post(createOrganization);
 router.route("/organizations").get(getAllOrganization);
 router.route("/organization/:id").get(getSingleOrganization);
-router.route("/recycler/:id").delete(deleteOrganization);
+router.route("/organization/:id").delete(deleteOrganization);
 
 module.exports = router;
